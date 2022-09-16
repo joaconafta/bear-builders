1f6d18664cdbca43b7f17be5b9c84d79eeffaa93
Luciano Giraudi <luciano.giraudi@itrocksweb.com>
2022-09-16 18:16:19 -0300

    pull

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,16 +1,224 @@
 import React from 'react'
 import styles from './displayHome.module.scss'
+import { Rating } from '@mui/material'
 const DisplayHome = () => {
     return (
         <div className={styles.layout}>
+            <div className={styles.featureds}>
+                <h2>Recommended Profiles</h2>
             <div className={styles.featuredFeed}>
-
+            <div className={styles.profile}>
+                    <p>Joaquin Naftaly</p>
+                    <div></div>
+                    <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+                </div>
+                <div className={styles.profile}>
+                    <p>Joaquin Naftaly</p>
+                    <div></div>
+                    <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+                </div>
+                <div className={styles.profile}>
+                    <p>Joaquin Naftaly</p>
+                    <div></div>
+                    <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+                </div>
+                <div className={styles.profile}>
+                    <p>Joaquin Naftaly</p>
+                    <div></div>
+                    <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+                </div>
+                <div className={styles.profile}>
+                    <p>Joaquin Naftaly</p>
+                    <div></div>
+                    <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+                </div>
+                <div className={styles.profile}>
+                    <p>Joaquin Naftaly</p>
+                    <div></div>
+                    <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+                </div>
+                <div className={styles.profile}>
+                    <p>Joaquin Naftaly</p>
+                    <div></div>
+                    <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+                </div>
+                <div className={styles.profile}>
+                    <p>Joaquin Naftaly</p>
+                    <div></div>
+                    <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+                </div>
+                <div className={styles.profile}>
+                    <p>Joaquin Naftaly</p>
+                    <div></div>
+                    <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+                </div>
             </div>
-            <div className={styles.feed}>
+               
+            </div>
+            
+            <div className={styles.commonFeed}>
+            <h2>Last <br/>Reviews</h2>
+            <div className={styles.featuredFeed}>
                 <div className={styles.cardsFeed}>
-                    <div className={styles.cardsFeedPic}></div>
-                    <div className={styles.cardsFeedTitle}></div>
-                    <div className={styles.cardsFeedDesc}></div>
+                    <div className={styles.cardsFeedPic}>
+                    <div>
+
+                    </div>
+                    <p>Joaquin Naftaly</p>
+                    </div>
+                    <div className={styles.cardsFeedTitle}>
+
+                        <p>Bear-Hackathon</p>
+                    <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+                    </div>
+                    <div className={styles.cardsFeedDesc}>
+                        <p>Muy buen evento, bien organizado, mucha comida, las oficinas estaban muy buenas, volveria a participar.</p>
+                    </div>
+                </div>
+                <div className={styles.cardsFeed}>
+                    <div className={styles.cardsFeedPic}>
+                    <div>
+
+                    </div>
+                    <p>Joaquin Naftaly</p>
+                    </div>
+                    <div className={styles.cardsFeedTitle}>
+
+                        <p>Bear-Hackathon</p>
+                    <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+                    </div>
+                    <div className={styles.cardsFeedDesc}>
+                        <p>Muy buen evento, bien organizado, mucha comida, las oficinas estaban muy buenas, volveria a participar.</p>
+                    </div>
+                </div>
+                <div className={styles.cardsFeed}>
+                    <div className={styles.cardsFeedPic}>
+                    <div>
+
+                    </div>
+                    <p>Joaquin Naftaly</p>
+                    </div>
+                    <div className={styles.cardsFeedTitle}>
+
+                        <p>Bear-Hackathon</p>
+                    <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+                    </div>
+                    <div className={styles.cardsFeedDesc}>
+                        <p>Muy buen evento, bien organizado, mucha comida, las oficinas estaban muy buenas, volveria a participar.</p>
+                    </div>
+                </div>
+                <div className={styles.cardsFeed}>
+                    <div className={styles.cardsFeedPic}>
+                    <div>
+
+                    </div>
+                    <p>Joaquin Naftaly</p>
+                    </div>
+                    <div className={styles.cardsFeedTitle}>
+
+                        <p>Bear-Hackathon</p>
+                    <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+                    </div>
+                    <div className={styles.cardsFeedDesc}>
+                        <p>Muy buen evento, bien organizado, mucha comida, las oficinas estaban muy buenas, volveria a participar.</p>
+                    </div>
+                </div>
+                <div className={styles.cardsFeed}>
+                    <div className={styles.cardsFeedPic}>
+                    <div>
+
+                    </div>
+                    <p>Joaquin Naftaly</p>
+                    </div>
+                    <div className={styles.cardsFeedTitle}>
+
+                        <p>Bear-Hackathon</p>
+                    <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+                    </div>
+                    <div className={styles.cardsFeedDesc}>
+                        <p>Muy buen evento, bien organizado, mucha comida, las oficinas estaban muy buenas, volveria a participar.</p>
+                    </div>
+                </div>
+                <div className={styles.cardsFeed}>
+                    <div className={styles.cardsFeedPic}>
+                    <div>
+
+                    </div>
+                    <p>Joaquin Naftaly</p>
+                    </div>
+                    <div className={styles.cardsFeedTitle}>
+
+                        <p>Bear-Hackathon</p>
+                    <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+                    </div>
+                    <div className={styles.cardsFeedDesc}>
+                        <p>Muy buen evento, bien organizado, mucha comida, las oficinas estaban muy buenas, volveria a participar.</p>
+                    </div>
+                </div>
+                <div className={styles.cardsFeed}>
+                    <div className={styles.cardsFeedPic}>
+                    <div>
+
+                    </div>
+                    <p>Joaquin Naftaly</p>
+                    </div>
+                    <div className={styles.cardsFeedTitle}>
+
+                        <p>Bear-Hackathon</p>
+                    <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+                    </div>
+                    <div className={styles.cardsFeedDesc}>
+                        <p>Muy buen evento, bien organizado, mucha comida, las oficinas estaban muy buenas, volveria a participar.</p>
+                    </div>
+                </div>
+                <div className={styles.cardsFeed}>
+                    <div className={styles.cardsFeedPic}>
+                    <div>
+
+                    </div>
+                    <p>Joaquin Naftaly</p>
+                    </div>
+                    <div className={styles.cardsFeedTitle}>
+
+                        <p>Bear-Hackathon</p>
+                    <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+                    </div>
+                    <div className={styles.cardsFeedDesc}>
+                        <p>Muy buen evento, bien organizado, mucha comida, las oficinas estaban muy buenas, volveria a participar.</p>
+                    </div>
+                </div>
+                <div className={styles.cardsFeed}>
+                    <div className={styles.cardsFeedPic}>
+                    <div>
+
+                    </div>
+                    <p>Joaquin Naftaly</p>
+                    </div>
+                    <div className={styles.cardsFeedTitle}>
+
+                        <p>Bear-Hackathon</p>
+                    <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+                    </div>
+                    <div className={styles.cardsFeedDesc}>
+                        <p>Muy buen evento, bien organizado, mucha comida, las oficinas estaban muy buenas, volveria a participar.</p>
+                    </div>
+                </div>
+                <div className={styles.cardsFeed}>
+                    <div className={styles.cardsFeedPic}>
+                    <div>
+
+                    </div>
+                    <p>Joaquin Naftaly</p>
+                    </div>
+                    <div className={styles.cardsFeedTitle}>
+
+                        <p>Bear-Hackathon</p>
+                    <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+                    </div>
+                    <div className={styles.cardsFeedDesc}>
+                        <p>Muy buen evento, bien organizado, mucha comida, las oficinas estaban muy buenas, volveria a participar.</p>
+                    </div>
+                </div>
                 </div>
             </div>
         </div>
---
+++ src/components/LoginModal.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import syles from './loginModal.module.scss'
+import Logo from '../assets/starlensLogo.png'
+const LoginModal = () => {
+  return (
+    <div>
+        <div>
+    <img src={Logo} alt=""  width='100'/>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default LoginModal
