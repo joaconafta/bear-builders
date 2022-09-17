5d4adc1ca2cd6abd0dd62560ef5e4cb7fd2efe34
Luciano Giraudi <luciano.giraudi@itrocksweb.com>
2022-09-16 22:55:06 -0300

    pull

--- src/pages/MemoDetail.jsx
+++ src/pages/MemoDetail.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import styles from './memoDetail.module.scss'
+import { Rating } from '@mui/material'
+import Previews from '../components/Previews'
+import StarModal from '../components/StarModal'
 const MemoDetail = () => {
+const [starModal, setStarModal] = useState(false)
   return (
-    <div>MemoDetail</div>
+    <div className={styles.wrapper}>
+      {starModal && <StarModal/>}
+    <div className={styles.profileHeader}>
+      <div className={styles.profilePic}>
+
+      </div>
+      
+        <div><p>Bear-Hackaton</p></div>
+        <div>
+          <button>Star This</button>
+          <Rating name="half-rating" defaultValue={2.5} precision={0.5}  size='large' className={styles.starSize}/>
+          </div>
+          <div className='desc'>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est voluptate cum iste iure nobis perspiciatis illo nostrum recusandae saepe quia, modi pariatur voluptatem, eligendi quasi. Quidem quae optio cupiditate ab?</p>
+            <div><p>Created by <span>Joaco Naftaly</span></p></div>
+          </div>
+    </div>
+    <Previews/>
+  </div>
   )
 }
 
