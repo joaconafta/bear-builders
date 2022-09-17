a70e154ba0d664bd5817a9ac08f46e85fb0ddfd7
Iván Lisas <ivan.lisas@itrocksweb.com>
2022-09-17 13:57:13 -0300

    commit

--- src/services/IpfsService.ts
+++ src/services/IpfsService.ts
@@ -25,19 +25,24 @@ export const uploadToIpfs = async (title: string, category: string, body: string
     description: category,
     content: body,
     external_url: null,
-    animation_url: '',
-    image: '',
-    cover: '',
-    imageMimeType: '',
+    animation_url: 'https://ipfs.infura.io/ipfs/QmNMYMMBiMWbPUJKU6M2dRUScX4xniB99eKVRrtERqT96i',
+    image: 'https://ipfs.infura.io/ipfs/QmWHB8e3MdxcVMxGLnB1N1ZsCSPPuuBWJseEZ6tP3YaKZn',
+    cover: 'https://ipfs.infura.io/ipfs/QmWHB8e3MdxcVMxGLnB1N1ZsCSPPuuBWJseEZ6tP3YaKZn',
+    imageMimeType: 'image/jpeg',
     name: title,
     attributes: [{ displayType: 'string', traitType: 'Publication', value: 'LenstubeVideo' }],
-    media: [],
+    media: [
+      { item: 'https://ipfs.infura.io/ipfs/QmNMYMMBiMWbPUJKU6M2dRUScX4xniB99eKVRrtERqT96i', type: 'video/mp4' },
+      { item: 'https://livepeercdn.com/asset/746b9zxagtln4u54/video', type: 'video/mp4' }
+    ],
     appId: 'Lenstube'
   }
 
   console.log('hola')
   const result = await (ipfs as IPFSHTTPClient).add(JSON.stringify(metadata))
-  return result.cid.toString()
+  console.log('result', result, result.path)
+  /* return 'ipfs://QmQgv6rWdBe28fiqxkC88LTPKYHGVHp2Y9DvSF35tRgk4M' */
+  return result.path
   //   console.log(result)
   // }
 }
