94e0c3cf2b6bf45fc18f18adbec2ee3ea0fbe277
Joaco <joaquin.naftaly@itrocksweb.com>
2022-09-16 23:52:35 -0300

    follow

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Box } from '@mui/system'
 import { createPost } from '../services/PostService'
+import { follow } from '../services/FollowService'
 import { Button } from '@mui/material'
 
 const Post: React.FC = () => {
@@ -8,11 +9,16 @@ const Post: React.FC = () => {
   const handlePost = async () => {
     await createPost()
   }
+  const handleFollow = async () => {
+    await follow('0x46ba')
+  }
 
   return (
     <Box >
       Post:
         <Button onClick={() => handlePost()}>Create Post</Button>
+      Follow:
+        <Button onClick={() => handleFollow()}>Follow</Button>
     </Box>
   )
 }
