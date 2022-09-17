f7b1027e251e20cb86e9223ed1828e3b61a68838
Luciano Giraudi <luciano.giraudi@itrocksweb.com>
2022-09-16 23:34:08 -0300

    pull

--- src/components/NewMomment.jsx
+++ src/components/NewMomment.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import styles from './NewMomment.module.scss'
+import styles from './newMomment.module.scss'
+import { Box, TextField, Select, SelectChangeEvent, Checkbox, ListItemText, MenuItem, OutlinedInput } from '@mui/material'
 const NewMomment = () => {
   return (
     <div className={styles.modalContainer}>
         <div className={styles.modal}>
-
+        <div>
+        <TextField id="outlined-basic" label="Search" variant="outlined" size="small" className={styles.input} />
+        </div>
+        <TextField id="outlined-basic" label="Search" variant="outlined" size="small" className={styles.input} />
+        </div>
+        <div>
+        <TextField
+  placeholder="MultiLine with rows: 2 and rowsMax: 4"
+  multiline
+  rows={2}
+  maxRows={4}
+/>
         </div>
     </div>
   )
